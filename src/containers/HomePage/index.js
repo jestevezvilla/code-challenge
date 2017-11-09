356b94fe394de53fefd667a4cc72f86f94b10284
jestevezvilla <jestevez.villa@gmail.com>
2017-11-09 12:25:31 +0100

    Apollo & Redux living together

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
+import { connect } from 'react-redux';
+
+import { deleteArticleAction } from '../../actions';
 
 import { ARTICLES_QUERY } from '../../actions/queries';
 import Card from '../../components/Card';
 import Loader from '../../components/Loader';
 import './styles.css';
 
-const HomePage = ({ data: { loading, error, articles } }) =>
+const HomePage = ({ data: { loading, error, articles }, onDeleteClick }) =>
   (
     <div className="HomePage">
       {error &&
@@ -19,7 +22,7 @@ const HomePage = ({ data: { loading, error, articles } }) =>
         <div className="HomePage__container">
           {articles.map(item =>
             (<Card
-              // onDelete={() => onDeleteClick(item.id)}
+              onDelete={() => onDeleteClick(item.id)}
               key={item.id}
               {...item}
             />))}
@@ -30,8 +33,17 @@ const HomePage = ({ data: { loading, error, articles } }) =>
 
 HomePage.propTypes = {
   data: PropTypes.oneOfType([PropTypes.object]),
+  onDeleteClick: PropTypes.func,
 };
 
+const mapDispatchToProps = dispatch => ({
+  onDeleteClick(id) {
+    dispatch(deleteArticleAction(id));
+  },
+});
+
+const HomePageConnected = connect(null, mapDispatchToProps)(HomePage);
+
 const articlesListQuery = gql`${ARTICLES_QUERY}`;
-const ArticlesListWithData = graphql(articlesListQuery)(HomePage);
+const ArticlesListWithData = graphql(articlesListQuery)(HomePageConnected);
 export default ArticlesListWithData;
